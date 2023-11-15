6c0fdb0c08fd2c27cbc0416c8d52efbf96fabcf0
casmidi <casmidiasli@yahoo.co.id>
2023-11-15 20:39:02 +0700

    commit turorial2

---
+++ batch_november/tutorial2_dart/bin/tutorial2_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tutorial2_dart/tutorial2_dart.dart' as tutorial2_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutorial2_dart.calculate()}!');
+}
---
+++ batch_november/tutorial2_dart/test/tutorial2_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutorial2_dart/tutorial2_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
